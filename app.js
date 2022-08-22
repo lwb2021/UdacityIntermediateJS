3febcf191006f45475dffa04aeca7f1471b637ca
Barry Ke <lbke2008@gmail.com>
2022-08-22 13:36:08 +0800

    add compare methods;

--- app.js
+++ app.js
@@ -1,4 +1,77 @@
-// import { Dinos } from './dino.json';
+const rawDinos = [
+	{
+			"species": "Triceratops",
+			"weight": 13000,
+			"height": 114,
+			"diet": "herbavor",
+			"where": "North America",
+			"when": "Late Cretaceous",
+			"fact": "First discovered in 1889 by Othniel Charles Marsh"
+	},
+	{
+			"species": "Tyrannosaurus Rex",
+			"weight": 11905,
+			"height": 144,
+			"diet": "carnivor",
+			"where": "North America",
+			"when": "Late Cretaceous",
+			"fact": "The largest known skull measures in at 5 feet long."
+	},
+	{
+			"species": "Anklyosaurus",
+			"weight": 10500,
+			"height": 55,
+			"diet": "herbavor",
+			"where": "North America",
+			"when": "Late Cretaceous",
+			"fact": "Anklyosaurus survived for approximately 135 million years."
+	},
+	{
+			"species": "Brachiosaurus",
+			"weight": 70000,
+			"height": "372",
+			"diet": "herbavor",
+			"where": "North America",
+			"when": "Late Jurasic",
+			"fact": "An asteroid was named 9954 Brachiosaurus in 1991."
+	},
+	{
+			"species": "Stegosaurus",
+			"weight": 11600,
+			"height": 79,
+			"diet": "herbavor",
+			"where": "North America, Europe, Asia",
+			"when": "Late Jurasic to Early Cretaceous",
+			"fact": "The Stegosaurus had between 17 and 22 seperate places and flat spines."
+	},
+	{
+			"species": "Elasmosaurus",
+			"weight": 16000,
+			"height": 59,
+			"diet": "carnivor",
+			"where": "North America",
+			"when": "Late Cretaceous",
+			"fact": "Elasmosaurus was a marine reptile first discovered in Kansas."
+	},
+	{
+			"species": "Pteranodon",
+			"weight": 44,
+			"height": 20,
+			"diet": "carnivor",
+			"where": "North America",
+			"when": "Late Cretaceous",
+			"fact": "Actually a flying reptile, the Pteranodon is not a dinosaur."
+	},
+	{
+			"species": "Pigeon",
+			"weight": 0.5,
+			"height": 9,
+			"diet": "herbavor",
+			"where": "World Wide",
+			"when": "Holocene",
+			"fact": "All birds are living dinosaurs."
+	}
+]
 
 // Create Dino Constructor
 function Dino(dinoInfo) {
@@ -13,11 +86,9 @@ function Dino(dinoInfo) {
 }
 
 // Create Dino Objects
-(function createDinoObjects() {
-	const data = require( "./dino.json").Dinos;
-	console.log('data ', data);
+(function createDinoObjects() {	
 	const arrayDinos = [];
-	for (const dino of data) {
+	for (const dino of rawDinos) {
 		arrayDinos.push(new Dino(dino));
 	}	
 	window.arrayDinos = arrayDinos;
@@ -38,41 +109,62 @@ function Human(humanInfo) {
 	const weight = document.getElementById("weight"); 
 	const diet = document.getElementById("diet"); 
 	const button = document.getElementById("btn"); 
-	
-	const enteredInfo = {
-		name: nameInput.value,
-		feetHeight: feetHeight.value,
-		inchHeight: inchHeight.value,
-		weight: weight.value,
-		diet: diet.value
-	};
-	console.log(123);
-	window.enteredInfo = enteredInfo;
-	console.log('d ', Dinos);
+
 	
 	button.addEventListener('click', () => {
 		// hideForm();
 		// prepareData();
 		// showTiles();
-		console.log(this);
+		const userInfo = {
+			name: nameInput.value,
+			feetHeight: feetHeight.value,
+			inchHeight: inchHeight.value,
+			weight: weight.value,
+			diet: diet.value
+		};
+
+		window.userInfo = userInfo;
+		const heightDiff = compareHeight();
+		const weightDiff = compareWeight();
+		const dietDiff = compareDiet();
+		console.log('height ', heightDiff);
+		console.log('weight ', weightDiff);
+		console.log('diet ', dietDiff);
 	});
 })();
 
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches. 
 function compareHeight() {
-	console.log();
+	const heightDiff = {};
+	for (const dino of arrayDinos) {
+		heightDiff[dino.species] = (dino.height * 12 - userInfo.feetHeight * 12 - userInfo.inchHeight) / 12;
+	}
+	return heightDiff;
 }
 
 // Create Dino Compare Method 2
 // NOTE: Weight in JSON file is in lbs, height in inches.
-
+function compareWeight() {
+	const weightDiff = {};
+	for (const dino of arrayDinos) {
+		weightDiff[dino.species] = dino.weight - userInfo.weight;
+	}
+	return weightDiff;
+}
 
 // Create Dino Compare Method 3
 // NOTE: Weight in JSON file is in lbs, height in inches.
-
+function compareDiet() {
+	const dietDiff = {};
+	for (const dino of arrayDinos) {
+		dietDiff[dino.species] = dino.diet;
+	}
+	return dietDiff;
+}
 
 // Generate Tiles for each Dino in Array
+
 
 // Add tiles to DOM
 
